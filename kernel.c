c7f5c84dd755c4e9f546aeabf5be36e56bbc4f8c
Hamsan2013 <hamsandivya@gmail.com>
2026-03-25 07:44:52 +0530

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,21 +1,11 @@
 void _start()
 {
-    char *video = (char*)0xB8000;
+    unsigned char *video = (unsigned char*)0xA0000;
 
-    video[0] = 'O';
-    video[1] = 0x0F;
+    for(int i = 0; i < 320*200; i++)
+    {
+        video[i] = 4;   // red screen
+    }
 
-    video[2] = 'S';
-    video[3] = 0x0F;
-
-    video[4] = ' ';
-    video[5] = 0x0F;
-
-    video[6] = 'O';
-    video[7] = 0x0F;
-
-    video[8] = 'K';
-    video[9] = 0x0F;
-
-    while(1){}
+    while(1) { }
 }
