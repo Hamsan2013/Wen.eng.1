14f79e07b2497d380711abb2d44c3b8e11c348c2
Hamsan2013 <hamsandivya@gmail.com>
2026-03-25 07:33:06 +0530

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,50 +1,21 @@
-void kernel_main();
-
 void _start()
 {
-    kernel_main();
-}
-#define FB 0xA0000
-#define W 320
-#define H 200
+    char *video = (char*)0xB8000;
 
-int unlocked=0;
-int screen=0;
+    video[0] = 'O';
+    video[1] = 0x0F;
 
-void plot(int x,int y,char c){
-((char*)FB)[y*W+x]=c;
-}
+    video[2] = 'S';
+    video[3] = 0x0F;
 
-void clear(char c){
-for(int i=0;i<W*H;i++)
-((char*)FB)[i]=c;
-}
+    video[4] = ' ';
+    video[5] = 0x0F;
 
-void draw_rect(int x,int y,int w,int h,char c){
-for(int j=y;j<y+h;j++)
-for(int i=x;i<x+w;i++)
-plot(i,j,c);
-}
+    video[6] = 'O';
+    video[7] = 0x0F;
 
-extern void render_lock();
-extern void render_desktop();
-extern void render_store();
+    video[8] = 'K';
+    video[9] = 0x0F;
 
-void kernel_main(){
-
-while(1){
-
-if(unlocked==0)
-render_lock();
-
-else if(screen==0)
-render_desktop();
-
-else
-render_store();
-
-asm("hlt");
-
-}
-
+    while(1){}
 }
